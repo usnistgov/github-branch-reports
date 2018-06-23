e45daf6365a3b960812cd043d5c2b516856f7d7b
MartyBurns <marty@hypertek.us>
2018-06-23 13:42:41 -0400

    script to fetch issues from all ucef subprojects

---
+++ fetchissues.sh
@@ -0,0 +1,6 @@
+bin/github-issue-reports.js -o usnistgov -r ucef -t ef783fb7353dc8fe6342453a90f1a98b7eb669e0
+bin/github-issue-reports.js -o usnistgov -r ucef-core -t ef783fb7353dc8fe6342453a90f1a98b7eb669e0
+bin/github-issue-reports.js -o usnistgov -r ucef-cpp -t ef783fb7353dc8fe6342453a90f1a98b7eb669e0
+bin/github-issue-reports.js -o usnistgov -r ucef-samples -t ef783fb7353dc8fe6342453a90f1a98b7eb669e0
+bin/github-issue-reports.js -o usnistgov -r ucef-meta -t ef783fb7353dc8fe6342453a90f1a98b7eb669e0
+bin/github-issue-reports.js -o usnistgov -r ucef-gridlabd-meta -t ef783fb7353dc8fe6342453a90f1a98b7eb669e0
